153a245acbe218d6e5c8783a9d7ba8762f1569f9
Dave Allen <spondicious@me.com>
2023-08-06 16:46:45 +0200

    Just after starting tutorial number 3 in the series.

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="name"),
+    path("<int:year>/<str:month>/", views.home, name="name"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,37 @@
+from os import walk
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 
-def home(request):
-    return render(request, "home.html", {})
+def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
+    name = "David"
+    # in case the month is added all in lower case. this makes it title case
+    month = month.capitalize()
+    # Convert month from string to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)  # make sure it is an int
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # get datetime
+    now = datetime.now()
+    current_year = now.year
+
+    # Get current time
+    time = now.strftime("%H:%M:%S")
+
+    return render(
+        request,
+        "home.html",
+        {
+            "first_name": name,
+            "year": year,
+            "month": month,
+            "month_number": month_number,
+            "cal": cal,
+            "current_year": current_year,
+            "time": time,
+        },
+    )
