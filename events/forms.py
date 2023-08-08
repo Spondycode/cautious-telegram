2b97f07cb47bcff29fef8f12926002cb95cfadc8
Dave Allen <spondicious@me.com>
2023-08-08 23:17:52 +0200

    After finishing the tut 8

--- events/forms.py
+++ events/forms.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from django import forms
 from django.forms import ModelForm
 from .models import Venue
@@ -15,3 +16,34 @@ class VenueForm(ModelForm):
             "web",
             "email_address",
         )
+        labels = {
+            "name": "",
+            "address": "",
+            "zip_code": "",
+            "phone": "",
+            "web": "",
+            "email_address": "",
+        }
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Name of the venue"}
+            ),
+            "address": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Venue Address Here"}
+            ),
+            "zip_code": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Postcode"}
+            ),
+            "phone": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Phone Number"}
+            ),
+            "web": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Web Address"}
+            ),
+            "email_address": forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Email Address for the Venue",
+                }
+            ),
+        }
--- events/views.py
+++ events/views.py
@@ -3,12 +3,35 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
-from .models import Event
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
 from .forms import VenueForm
 
 
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, "events/show_venue.html", {"venue": venue})
+
+
+def list_venues(request):
+    venue_list = Venue.objects.all()
+    return render(request, "events/venues.html", {"venue_list": venue_list})
+
+
 def add_venue(request):
-    return render(request, "events/add_venue.html", {})
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/add_venue?submitted=True")
+    else:
+        form = VenueForm
+        if "submitted" in request.GET:
+            submitted = True
+    return render(
+        request, "events/add_venue.html", {"form": form, "submitted": submitted}
+    )
 
 
 def all_events(request):
